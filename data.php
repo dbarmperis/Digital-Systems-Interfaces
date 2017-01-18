a2d7aea2d110a85115ace6d118e7e6fa3b8c0256
Dimitrios Barmperis <dimbarmperis@gmail.com>
2017-01-19 00:39:31 +0200

    Arduino and php code

---
+++ data.php
@@ -0,0 +1,3 @@
+<?php
+$time = 'Wednesday 18th of January 2017 11:00:20 PM';$temp = 19;$door = 0;$smoke = 0;$motion = 0;$last_door = 'Wednesday 18th of January 2017 10:58:17 PM';$last_smoke='Wednesday 18th of January 2017 11:00:09 PM';$last_motion='Wednesday 18th of January 2017 10:59:24 PM';
+?>
